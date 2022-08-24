93a398b71331320a409c34198926c869e2ca9dcb
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-24 14:31:41 +0900

    complete creating sign up page and set up body-parser

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const port = 3000;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res)=>{
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    console.log("Fist name: " + firstName);
+    console.log("Last name: " + lastName);
+    console.log("Email: " + email);
+
+    res.sendFile(__dirname + "/success.html");
 });
 
 app.listen(port, ()=>{
